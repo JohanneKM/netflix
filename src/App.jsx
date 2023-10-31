1da4f118e58aa42b5a866bf84fa9370afbc47ac2
Johanne Khan Malek <johannekhanmalek@Johannes-MacBook-Pro.local>
2023-10-31 21:51:20 +0100

    with-component-Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
 
 import netflix from "./assets/netflix.json";
+import Section from "./components/Section";
 
 function App() {
-  const netflixComedies = netflix[1].images[0];
+  // const netflixComedies = netflix[1].images[0];
 
   // Le tableau d'objet Netflix n'est pas affichable par react
 
@@ -13,26 +14,23 @@ function App() {
     // console.log(elem);
 
     return (
-      <section>
-        <p>{elem.category}</p>
+      <>
+        <section>
+          <p>{elem.category}</p>
 
-        <div className="pictures">
-          {elem.images.map((elem2) => {
-            return <img src={elem2} />;
-          })}
-        </div>
-      </section>
+          <div className="pictures">
+            {elem.images.map((elem2) => {
+              return <img src={elem2} />;
+            })}
+          </div>
+        </section>
+      </>
     );
   });
 
   // J'utilise map créer un nouveau tableau d'objets [ [url1, url2, etc] , [url3, url 4, url 5, etc], [url 6, url7, url 8, etc], etc]
 
-  const arrayOfImagesMadeWithMap = netflix.map((elem) => {
-    console.log(elem);
-    return <p> {elem.images} </p>;
-  });
-
-  return <>{tabMadeWithMap};</>;
+  return <>{tabMadeWithMap}</>;
 }
 
 export default App;
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,6 +1,23 @@
-const Section = (props) => {
+import netflix from "../assets/netflix.json";
 
-    return ()
-}
+const Section = () => {
+  return (
+    <>
+      {netflix.map((elem) => {
+        return (
+          <section>
+            <h2> {elem.category} </h2>;
+            <div>
+              {elem.images.map((elem2) => {
+                return <img src={elem2} />;
+              })}
+            </div>
+          </section>
+        );
+      })}
+      ;
+    </>
+  );
+};
 
 export default Section;
